ed14e4fad834343db9aa01ed5700c17338ad03bd
Zeeshan Ahmad <zeems1234@gmail.com>
2019-11-03 23:59:17 +0500

    Updates

---
+++ precache-manifest.66440d57a2a3cc692bd5e696a0b08838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ee55762415db73fa5f2c08d4d4d9a8",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "0086856d2477b643986f",
+    "url": "/robo-friends/static/css/main.f872656e.chunk.css"
+  },
+  {
+    "revision": "6fcafa3ed38d492b6406",
+    "url": "/robo-friends/static/js/2.0d6322b5.chunk.js"
+  },
+  {
+    "revision": "0086856d2477b643986f",
+    "url": "/robo-friends/static/js/main.d0eec302.chunk.js"
+  },
+  {
+    "revision": "7798536cf61677f5ca83",
+    "url": "/robo-friends/static/js/runtime-main.40bdca23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
